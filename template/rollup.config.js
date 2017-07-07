0662db6be99cf368fc3984cd6fb6985f93602fd1
dwqs <461147874@qq.com>
2017-07-07 14:42:47 +0800

    [misc]update script config

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,38 +1,30 @@
-/**
- * Created by pomy on 03/07/2017.
- */
-
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import builtins from 'rollup-plugin-node-builtins';
 
 export default {
-    entry: 'src/test.ts',
+    entry: 'src/index.ts',
     format: 'umd',
     moduleName: 'test',
-    dest: 'dist/test.min.js',
+    dest: 'dist/test.js',
     plugins: [
         builtins(),
         resolve({
             customResolveOptions: 'node_modules',
             jsnext: true
         }),
-        commonjs(),
+        commonjs({
+            // https://github.com/rollup/rollup-plugin-commonjs#custom-named-exports
+            namedExports: {
+                
+            }
+        }),
         typescript(),
         babel({
             exclude: 'node_modules/**',
             externalHelpers: true
-        }),
-        uglify({
-            compress: {
-                warnings: false,
-                drop_debugger: true,
-                drop_console: true
-            },
-            mangle: true
         })
     ]
 }
